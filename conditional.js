f7bef8d904596eea9cda275ea9d73a690a6567d4
JANANI-D2006 <jananid.23eee@kongu.edu>
2025-06-19 11:35:40 +0530

    complete conditional

---
+++ conditional.js
@@ -0,0 +1,10 @@
+let marks=85;
+if(marks >= 90){
+    console.log("a grade");
+}
+else if(marks>=75){
+    console.log("b grade");
+}
+else{
+    console.log("c grade");
+}
---
+++ loops.js
@@ -0,0 +1,11 @@
+let a=0;
+while(a < 10){
+    if(a==5){
+        continue;
+    }
+    if(a==8){
+        break;
+    }
+    console.log(a);
+    a++;
+}
